422bd987c0fa2515400663e285e87fb80b5ad21e
Yupeng <li.yupeng.uk@gmail.com>
2020-03-15 18:28:37 +0000

    updated the todomvc example

--- examples/todomvc/src/index.js
+++ examples/todomvc/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import 'todomvc-app-css/index.css'
-import {ReduxManager, createState} from 'redux-simple-state'
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import "todomvc-app-css/index.css";
+import { ReduxManager } from "redux-simple-state";
 
-import todosController from './controllers/todosController'
-const store = ReduxManager.createStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import todosController from "./controllers/todosController";
+const store = ReduxManager.createStore(
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 ReduxManager.registerState(todosController.state);
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
